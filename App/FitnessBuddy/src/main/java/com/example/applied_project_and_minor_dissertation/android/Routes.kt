69a06e350406fb8791d7791757ef8dc6207e0ecb
swordslinger <seanskelton40@gmail.com>
2022-02-03 17:42:27 +0000

    fixed issues with routing registeration requests

--- App/FitnessBuddy/src/main/java/com/example/applied_project_and_minor_dissertation/android/Routes.kt
+++ App/FitnessBuddy/src/main/java/com/example/applied_project_and_minor_dissertation/android/Routes.kt
@@ -2,7 +2,7 @@ package com.example.applied_project_and_minor_dissertation.android
 
 class Routes {
     object HttpRoutes{
-        const val BASE_URL = "http://localhost:8080/"
+        const val BASE_URL = "http://192.168.1.1:8080/"
         const val REGISTER = "$BASE_URL" + "api/auth/signup"
     }
 }
