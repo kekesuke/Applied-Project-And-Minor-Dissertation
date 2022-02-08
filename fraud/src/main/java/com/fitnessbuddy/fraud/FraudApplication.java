75b075090f1d7382697356d878bec898a7bbf04a
kekesuke <emilparvanovv@gmail.com>
2022-02-08 21:01:38 +0000

    Zipkin tracing and ApiGateway added

--- fraud/src/main/java/com/fitnessbuddy/fraud/FraudApplication.java
+++ fraud/src/main/java/com/fitnessbuddy/fraud/FraudApplication.java
@@ -9,19 +9,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class FraudApplication implements CommandLineRunner {
-	private static final Logger log = LoggerFactory.getLogger(FraudApplication.class);
-
-
+public class FraudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FraudApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
-	
-
 
 }
