e889da6613d199d6b04d3b9061b96efbfeaa9234
swordslinger <seanskelton40@gmail.com>
2022-03-14 21:35:43 +0000

    testing something

--- App/FitnessBuddy/src/main/java/com/example/applied_project_and_minor_dissertation/android/LoginActivity.kt
+++ App/FitnessBuddy/src/main/java/com/example/applied_project_and_minor_dissertation/android/LoginActivity.kt
@@ -7,6 +7,7 @@ class  LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        "I need to test some shit"
 
     }
 
