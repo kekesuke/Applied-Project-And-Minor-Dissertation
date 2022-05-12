9d103727086513a8ab0c486770653c43233ae69f
FionnBrowne <fionnftb@gmail.com>
2022-05-12 11:07:39 +0100

    added BMI calculator

--- App/FitnessBuddy/src/main/java/com/example/applied_project_and_minor_dissertation/android/ui/diary/DiaryFragment.kt
+++ App/FitnessBuddy/src/main/java/com/example/applied_project_and_minor_dissertation/android/ui/diary/DiaryFragment.kt
@@ -1,133 +1,99 @@
 package com.example.applied_project_and_minor_dissertation.android.ui.diary
 
-import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.applied_project_and_minor_dissertation.android.R
+import kotlinx.android.synthetic.main.activity_diary.*
+import kotlin.math.pow
 
 
 class DiaryFragment : Fragment() {
 
-    private var sensorManager: SensorManager? = null
-
-    private var running = false
-    private var totalSteps = 0f
-    private var previousTotalSteps = 0f
-    private val ACTIVITY_RECOGNITION_REQUEST_CODE = 101
+    private var imageMale = image_male
+    private var imageGirl = image_female_blur
+    private var weight = weight_value
+    private var height = height_value
+    private var calculateButton = calculate_button
+    private var BMI = bmi
+    private var bmiStatus = bmi_status
+    private var BMIView = bmiView
+    private var calculateAgainButton = reCalculate
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_diary, container, false)
-    }
 
-    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        //check if permission isn't already granted, request the permission
-        if (isPermissionGranted()) {
-            requestPermission()
+        val root = inflater.inflate(R.layout.activity_diary, container, false)
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+        imageMale = root.findViewById(R.id.image_male)
+        imageGirl = root.findViewById(R.id.image_female_blur)
+        weight = root.findViewById(R.id.weight_value)
+        height = root.findViewById(R.id.height_value)
+        calculateButton = root.findViewById(R.id.calculate_button)
+        BMI = root.findViewById(R.id.bmi)
+        bmiStatus = root.findViewById(R.id.bmi_status)
+        BMIView = root.findViewById(R.id.bmiView)
+        calculateAgainButton = root.findViewById(R.id.reCalculate)
+
+        imageMale.setOnClickListener{
+            imageMale.setImageResource(R.drawable.ic_male)
+            imageGirl.setImageResource(R.drawable.ic_female_blur)
         }
-        //sensor code
-        sensorManager = requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        loadData()
-        resetSteps()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        running = true
-        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-
-        if(stepSensor == null){
-            Toast.makeText(requireContext(), "No sensor detected on this device", Toast.LENGTH_SHORT).show()
-        }else{
-            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)//delays checks to use less resources.
-            Toast.makeText(requireContext(), "sensor detected on this device", Toast.LENGTH_SHORT).show()
+        imageGirl.setOnClickListener {
+            imageMale.setImageResource(R.drawable.ic_male_faded)
+            imageGirl.setImageResource(R.drawable.ic_female)
         }
-    }
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        if(running){
-            totalSteps = event!!.values[0]
-            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
-            tv_stepsTaken.text = ("$currentSteps")
-
-            circularProgressBar.apply{
-                setProgressWithAnimation(currentSteps.toFloat())
+        calculateButton.setOnClickListener {
+            var weightValue = 0.0
+            var heightValue = 0.0
+            if (weight.text.toString().isNotEmpty()) {
+                weightValue = weight.text.toString().toDouble()
             }
-        }
-    }
-
-    private fun resetSteps(){
-        tv_stepsTaken.setOnClickListener{
-            Toast.makeText(requireContext(), "Long tap to reset steps", Toast.LENGTH_SHORT).show()
-
-        }
-        tv_stepsTaken.setOnLongClickListener{
-            previousTotalSteps = totalSteps
-            tv_stepsTaken.text = 0.toString()
-            saveData()
-
-            true
-        }
-    }
-    private fun saveData(){
-        //val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val sharedPreferences = requireContext().getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putFloat("key1", previousTotalSteps)
-        editor.apply()
-
-    }
-    private fun loadData(){
-        //val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val sharedPreferences = requireContext().getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val savedNumber = sharedPreferences.getFloat("key1", 0f)
-        Log.d("CaloriesFragment", "$savedNumber")
-        previousTotalSteps = savedNumber
-    }
-
-    private fun requestPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            ActivityCompat.requestPermissions(
-                requireContext() as Activity,
-                arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
-                ACTIVITY_RECOGNITION_REQUEST_CODE
-            )
-        }
-    }
-
-    private fun isPermissionGranted(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            requireContext(),
-            Manifest.permission.ACTIVITY_RECOGNITION
-        ) != PackageManager.PERMISSION_GRANTED
-    }
-
-    //handle requested permission result(allow or deny)
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            ACTIVITY_RECOGNITION_REQUEST_CODE -> {
-                if ((grantResults.isNotEmpty() &&
-                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                ) {
-                    // Permission is granted. Continue the action
-                }
+            if (height.text.toString().isNotEmpty()) {
+                heightValue = (height.text.toString().toDouble() / 100)
             }
+            if (weightValue > 0.0 && heightValue > 0.0) {
+                val bmiValue = String.format("%.2f", weightValue / heightValue.pow(2))
+                bmi.text = bmiValue
+                bmiStatus.text = bmiStatusValue(weightValue / heightValue.pow(2))
+                bmiView.visibility = VISIBLE
+                calculateButton.visibility = GONE
+            } else
+                Toast.makeText(activity, "Please Input valid Weight and Height Values greater than 0", Toast.LENGTH_LONG).show()
+
         }
+        calculateAgainButton.setOnClickListener {
+            bmiView.visibility = GONE
+            calculateButton.visibility = VISIBLE
+            weight.text.clear()
+            height.text.clear()
+            weight.requestFocus()
+        }
+
+        return root
     }
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        println("onAccuracyChanged: Sensor: $sensor; accuracy: $accuracy")
-    }*/
+    private fun bmiStatusValue(bmi: Double): String {
+        lateinit var bmiStatus: String
+        if (bmi < 18.5)
+            bmiStatus = "Underweight"
+        else if (bmi >= 18.5 && bmi < 25)
+            bmiStatus = "Normal"
+        else if (bmi >= 25 && bmi < 30)
+            bmiStatus = "Overweight"
+        else if (bmi > 30)
+            bmiStatus = "Obese"
+        return bmiStatus
+    }
 }
