2cbba08b002cb74295555e144754192b2202860a
swordslinger <seanskelton40@gmail.com>
2022-05-18 01:52:15 +0100

    set up adapter

---
+++ App/FitnessBuddy/src/main/java/com/example/applied_project_and_minor_dissertation/android/ui/chat/ChatAdapter.java
@@ -0,0 +1,44 @@
+package com.example.applied_project_and_minor_dissertation.android.ui.chat;
+
+import android.view.LayoutInflater;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.applied_project_and_minor_dissertation.android.R;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.SimpleViewHolder> {
+
+    private final List<String> dataSet;
+
+    public ChatAdapter(List<String> data) {dataSet = data;}
+
+    
+    @Override
+    public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new SimpleViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(SimpleViewHolder holder, int position) {
+        holder.textView.setText(dataSet.get(position));
+    }
+
+    @Override
+    public int getItemCount(){return dataSet.size();}
+
+    static class SimpleViewHolder extends RecyclerView.ViewHolder{
+        final TextView textView;
+
+        public SimpleViewHolder(View itemView){
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.text);
+        }
+    }
+}
